43dc3e27fd4391f5a612b7977e662ba9311d2598
uuutako <okn.sk009@gmail.com>
2020-12-15 11:14:59 +0900

    ユーザー登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  root'plans#index'  
+
+ 
+  resources :users, only: :new  
 end
